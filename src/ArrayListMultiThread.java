9748dac6fa6619f3b7084d7b2fe9d73775e7862f
ffismine <ffismine>
2020-06-17 11:36:23 +0800

    initial

---
+++ src/ArrayListMultiThread.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class ArrayListMultiThread {
+    static ArrayList<Integer> al = new ArrayList<Integer>(10);
+    public static class AddThread implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000000; i++){
+                al.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new AddThread());
+        Thread t2 = new Thread(new AddThread());
+        t1.start();
+        t2.start();
+        t1.join();t2.join();
+        System.out.println(al.size());
+    }
+}
+
+// 书上说三种情况：
+// 2000000；报错1000015；不报错1792353
+// 事实上每次都是1000000 - 1001000，以及第三种情况比较多
